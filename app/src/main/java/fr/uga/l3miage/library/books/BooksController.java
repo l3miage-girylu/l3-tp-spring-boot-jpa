b14e64f8a2058ee8724d1763897b89f90d73ea5e
l3miage-girylu <50694021+l3miage-girylu@users.noreply.github.com>
2023-03-05 23:12:21 +0100

    Update BooksController.java

--- app/src/main/java/fr/uga/l3miage/library/books/BooksController.java
+++ app/src/main/java/fr/uga/l3miage/library/books/BooksController.java
@@ -1,26 +1,30 @@
 package fr.uga.l3miage.library.books;
 
+import fr.uga.l3miage.data.domain.Author;
+import fr.uga.l3miage.data.domain.Book;
 import fr.uga.l3miage.library.authors.AuthorDTO;
+import fr.uga.l3miage.library.service.AuthorService;
 import fr.uga.l3miage.library.service.BookService;
+import fr.uga.l3miage.library.service.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Collection;
 
 @RestController
-@RequestMapping(value = "/api", produces = "application/json")
+@RequestMapping(value = "/api/v1", produces = "application/json")
 public class BooksController {
 
     private final BookService bookService;
     private final BooksMapper booksMapper;
-
+    private final AuthorService authorService;
     @Autowired
-    public BooksController(BookService bookService, BooksMapper booksMapper) {
+    public BooksController(BookService bookService, BooksMapper booksMapper,  AuthorService authorService) {
        this.bookService = bookService;
         this.booksMapper = booksMapper;
+        this.authorService = authorService;
     }
 
     @GetMapping("/books/v1")
@@ -28,21 +32,63 @@ public class BooksController {
         return null;
     }
 
-    public BookDTO book(Long id) {
-        return null;
+    @GetMapping("/books/{id}")
+    public BookDTO book(@PathVariable("id") Long id){
+        try{
+            var li = this.bookService.get(id);
+            return booksMapper.entityToDTO(li);
+        }catch(Exception e){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
     }
 
-    public BookDTO newBook(Long authorId, BookDTO book) {
-        return null;
+
+    @PostMapping("/authors/{authorId}/books")
+    @ResponseStatus(HttpStatus.CREATED)
+    public BookDTO newBook(@PathVariable("authorId") Long authorId, @RequestBody BookDTO book) {
+        try {
+            authorService.get(authorId);
+        } catch (EntityNotFoundException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Auteur non trouvé", e);
+        }
+        try {
+            if(book.title().replaceAll("\\s", "").equals("") || Long.toString(book.isbn()).length() < 10 || Long.toString(book.year()).length() > 4){
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+            }
+            var livre = this.booksMapper.dtoToEntity(book);
+            this.bookService.save(authorId, livre);
+            return this.booksMapper.entityToDTO(livre);
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
     }
 
     public BookDTO updateBook(Long authorId, BookDTO book) {
         // attention BookDTO.id() doit être égale à id, sinon la requête utilisateur est mauvaise
-        return null;
+
+        if(authorId == book.id()){
+            try{
+                var livre = this.bookService.get(book.id());
+                livre.setId(authorId);
+                return this.booksMapper.entityToDTO(livre);
+
+            }catch (Exception e){
+                throw new ResponseStatusException(HttpStatus.RESET_CONTENT);
+            }
+        }else{
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
     }
 
-    public void deleteBook(Long id) {
-
+    @DeleteMapping("/books/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteBook(@PathVariable("id") Long id) {
+        try{
+            Book livre = this.bookService.get(id);
+            this.bookService.delete(id);
+        }catch (Exception e){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
     }
 
     public void addAuthor(Long authorId, AuthorDTO author) {
